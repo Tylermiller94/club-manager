2be315e4148fef29a36f61269ac5b910a41b66f4
tylermiller94 <tylermiller94@gmail.com>
2017-11-03 11:20:31 -0700

    add initial files and firebase key

---
+++ src/app/api-keys.ts
@@ -0,0 +1,9 @@
+// Initialize Firebase
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyDNB9eeuilfOCKt5GZ1IfnbOPNXkzfWz5I",
+  authDomain: "club-manager-9185a.firebaseapp.com",
+  databaseURL: "https://club-manager-9185a.firebaseio.com",
+  projectId: "club-manager-9185a",
+  storageBucket: "",
+  messagingSenderId: "578686392113"
+};
